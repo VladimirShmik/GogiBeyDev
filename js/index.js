872645ec84ba328ea29d3704a36b674690e99478
vladimir <indigosunset00@gmail.com>
2022-08-04 06:08:39 +0600

    fixes

--- js/index.js
+++ js/index.js
@@ -12,13 +12,61 @@ new Swiper(".reviewsSlider", {
     spaceBetween: 15,
 
 });
- let clickButton = document.querySelector('.slider-block');
- let clickList = document.querySelector('.dropdown-block');
- ['mousedown', 'mouseup'].forEach(type => {
-    if (type === 'mousedown') {
-        clickButton.addEventListener(type, () => );
-    }
-    else if (type === 'mouseup') {
-        clickButton.addEventListener(type, () => console.log('Mouse is up'));
+new Swiper(".optionsSlider", {
+    slidesPerView: 4,
+    spaceBetween: 15,
+
+});
+new Swiper(".similarSlider", {
+    slidesPerView: 4,
+    spaceBetween: 15,
+
+});
+//popover-button//
+let popoverBtn = document.querySelector('.results-footer-btn');
+let popoverDropdown = document.querySelector('.results-dropdown');
+if (popoverBtn != null) {
+    popoverBtn.addEventListener('click', function () {
+            popoverDropdown.classList.toggle('results-dropdown--active');
+            setTimeout(function () {
+                popoverDropdown.classList.remove('results-dropdown--active');
+            }, 3000);
+        }
+    );
+};
+//search-icons//
+let searchContainer = document.querySelector('.search-content');
+    if (searchContainer != null) {
+     searchContainer.addEventListener('click', container);
+    function container(event) {
+        if (event.target.closest('.fav-btn')) {
+            event.target.classList.toggle('fav-btn--active');
+        };
+    };
+};
+
+let foundContainer = document.querySelector('.bottom-container');
+let foundBtn = document.querySelector('.film-except__btn');
+let foundText = document.querySelector('.film-except__text');
+if (foundContainer != null) {
+    foundContainer.addEventListener('click', bottomContainer);
+
+    function bottomContainer(event) {
+        if (event.target.closest('.film-except__btn')) {
+            foundBtn.classList.toggle('film-except__btn--active');
+            foundText.classList.toggle('film-except__text--active')
+        };
+    };
+};
+let sliderContainer = document.querySelector('.slider-box');
+let sliderDropdown = document.querySelector('.slider-dropdown');
+let mouseDownTime = 0 ;
+const clickTime = 1000;
+sliderContainer.addEventListener('mousedown', () =>{
+    mouseDownTime = Date.now();
+})
+sliderContainer.addEventListener('click', (event) => {
+    if(Date.now() - mouseDownTime < clickTime) {
+        sliderDropdown.classList.toggle('slider-dropdown--active')
     }
 });
